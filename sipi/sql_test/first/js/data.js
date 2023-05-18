a67a69a6962017485b4c3f286e7a408a39530be3
RinaSempai <aikorina806@gmail.com>
2023-05-19 02:41:17 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   sipi/blog.html
    	modified:   sipi/index.html
    	new file:   sipi/sql_test/first/css/app.css
    	new file:   sipi/sql_test/first/index.html
    	new file:   sipi/sql_test/first/js/app.js
    	new file:   sipi/sql_test/first/js/data.js
        git commit 'test'

---
+++ sipi/sql_test/first/js/data.js
@@ -0,0 +1,52 @@
+const ques = [
+    {
+        que: "Что означает акроним SQL?",
+        option: ["Structured Query Language", "Standard Query Language", "Standard Query Language", "Simple Query Language"],
+        crt: 0
+    },
+    {
+        que: "Какой тип базы данных используется в SQL?",
+        option: ["Иерархические базы данных", "Реляционные базы данных", "Графовые базы данных", "Объектно-ориентированные базы данных"],
+        crt: 1
+    },
+    {
+        que: "Какие операции можно выполнять с помощью SQL?",
+        option: ["Только чтение данных", "Создание, чтение, обновление и удаление данных", "Только создание данных", "Только удаление данных"],
+        crt: 1
+    },
+    {
+        que: "Какая команда используются для создания таблицы в SQL?",
+        option: ["INSERT INTO", "UPDATE", "DELETE", "CREATE TABLE"],
+        crt: 3
+    },
+    {
+        que: "Какая команда используется для выборки данных из таблицы?",
+        option: ["UPDATE", "INSERT INTO", "SELECT", "DELETE FROM"],
+        crt: 2
+    },
+    {
+        que: "Какая функция используется для подсчета количества записей в таблице?",
+        option: ["SUM", "MAX", "AVG", "COUNT"],
+        crt: 3
+    },
+    {
+        que: "Какая команда используется для добавления новой записи в таблицу??",
+        option: ["UPDATE", "SELECT", "DELETE FROM", "INSERT INTO"],
+        crt: 3
+    },
+    {
+        que: "Какая команда используется для изменения данных в таблице?",
+        option: ["INSERT INTO", "SELECT", "UPDATE", "DELETE FROM"],
+        crt: 2
+    },
+    {
+        que: "Какая команда используется для удаления записи из таблицы?",
+        option: ["INSERT INTO", "UPDATE", "DELETE FROM", "SELECT"],
+        crt: 2
+    },
+    {
+        que: "Какая команда используется для удаления таблицы из базы данных?",
+        option: ["DELETE TABLE", "REMOVE TABLE", "DROP TABLE", "ERASE TABLE"],
+        crt: 2
+    }
+];
